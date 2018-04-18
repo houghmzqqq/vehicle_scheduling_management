db6990129f4217b6f35bc09332a789b62004505f
houghmzqqq <yejunfeng95@163.com>
2018-04-18 17:33:21 +0800

    ..

--- src/main/java/com/example/vehicle_scheduling_management/service/Impl/LoginServiceImpl.java
+++ src/main/java/com/example/vehicle_scheduling_management/service/Impl/LoginServiceImpl.java
@@ -4,10 +4,12 @@ import com.example.vehicle_scheduling_management.mapper.UserInfoMapper;
 import com.example.vehicle_scheduling_management.pojo.UserInfo;
 import com.example.vehicle_scheduling_management.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by 叶俊锋 on 2018/4/18.
  */
+@Service
 public class LoginServiceImpl implements LoginService {
 
     @Autowired
